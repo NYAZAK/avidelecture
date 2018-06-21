dce3156646d6cdc221083bda7d13e043edeb7065
codeconcept <contact@codeconcept.fr>
2018-06-21 12:27:00 +0200

    adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { BookAddComponent } from './book-add/book-add.component';
+import { BookListComponent } from './book-list/book-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'actu', component: NewsComponent },
+  { path: 'ajouter-livre', component: BookAddComponent},
+  { path: 'mes-livres', component: BookListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CcSidenavComponent } from './cc-sidenav/cc-sidenav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { BookAddComponent } from './book-add/book-add.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CcSidenavComponent
+    CcSidenavComponent,
+    HomeComponent,
+    NewsComponent,
+    BookAddComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
